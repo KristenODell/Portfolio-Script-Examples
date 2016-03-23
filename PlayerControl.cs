b044d2231ac2452ff9f22f68ca7e16c7fd570d04
KristenODell <little_miss_kris07@hotmail.com>
2016-03-22 21:21:17 -0600

    Added files via upload

---
+++ PlayerControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    
+    public float rotationSpeed;
+    public float movementSpeed;
+    private Rigidbody variable1;
+
+     void Start()
+    {
+        variable1 = GetComponent<Rigidbody>();
+    }
+    
+     void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, -rotationSpeed, 0));
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, rotationSpeed, 0));
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            variable1.AddRelativeForce(0, 0, movementSpeed);
+        }
+    }
+   
+}
